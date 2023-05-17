12fa6ea9bd86ed1ca8675e62b991e1e2dfc7fed3
Sergey <svg95@lsit.ru>
2023-05-17 23:51:21 +0300

    lesson_3

--- library/authors/models.py
+++ library/authors/models.py
@@ -5,3 +5,22 @@ class Author(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     birthday_year = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.first_name
+
+
+class Biography(models.Model):
+    text = models.TextField()
+    author = models.OneToOneField(Author, on_delete=models.CASCADE)
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=32)
+    authors = models.ManyToManyField(Author)
+
+
+class Article(models.Model):
+    name = models.CharField(max_length=32)
+    text = models.TextField()
+    author = models.ForeignKey(Author, models.PROTECT)
--- library/authors/serializers.py
+++ library/authors/serializers.py
@@ -1,9 +1,27 @@
 from rest_framework.serializers import HyperlinkedModelSerializer
 
-from .models import Author
+from .models import Author, Biography, Article, Book
 
 
 class AuthorModelSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = Author
+        fields = '__all__'
+
+
+class BiographyModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = Biography
+        fields = ['text', 'author']
+
+
+class ArticleModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = Article
+        fields = ['name', 'text']
+
+
+class BookModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = Book
         fields = '__all__'
